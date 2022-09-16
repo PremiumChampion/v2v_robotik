2ecff349954b1ba2a6cce90347101d21022707aa
bkammer <bkammer@stud.hs-heilbronn.de>
2022-09-16 14:13:31 +0200

    Code implementation to setup a server and to connect to a server in case of a client.

--- v2v_esp32/src/communication/server/connection.cpp
+++ v2v_esp32/src/communication/server/connection.cpp
@@ -1,5 +1,20 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include "communication/client/socket/socket.h"
+
 //Creates the connection between server and client
+
+WiFiClient client;
+IPAddress server_ip(192, 168, 4, 1);
+int port = 80;
+
 namespace HHN_Client
 {
-    void createConnection() {} // create tcp client
+    void createConnection() {
+        client.connect(server_ip, port);
+        Serial.println("Connected to server");
+        
+        new HHN_Client::Socket(client);
+    }
 } // namespace Client
--- v2v_esp32/src/communication/server/server.cpp
+++ v2v_esp32/src/communication/server/server.cpp
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include "server.h"
+
 #include "communication/client/socket/socket.h"
 
 namespace WLAN
@@ -7,11 +8,13 @@ namespace WLAN
     HHN_Server::HHN_Server()
     {
         this->server = WiFiServer(80);
+        server.begin();
+        Serial.println("Server started!");
     }
 
     void HHN_Server::run()
     {
-        WiFiClient client = this->server.available();
+        WiFiClient client = this->server.available(); // Check if a new client connected
 
         if (client)
         {
