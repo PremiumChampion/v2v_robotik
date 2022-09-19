b8222e00d71208dfd7c6945d9b7927096225ba4c
bkammer <bkammer@stud.hs-heilbronn.de>
2022-09-19 12:29:30 +0200

    Moved definition of chaser/chased-one from wlan to communication.

--- v2v_esp32/src/communication/communication.cpp
+++ v2v_esp32/src/communication/communication.cpp
@@ -3,11 +3,20 @@
 
 // Communicaton setup file
 
+#define CHASER 0
+#define CHASED 1
+
+#define ROLE CHASED
+
 void setup()
 {
-    WLAN::setup();
 
-    // setup wifi
+#if ROLE == CHASED        // Chased-one
+    WLAN::setup_Client(); // Setup connection to the accesspoint
+#else                     // Chaser
+    WLAN::setup_AP(); // Create an accesspoint
+#endif
+
     // create server
     // connect to server
 }
--- v2v_esp32/src/communication/wlan/wlan.cpp
+++ v2v_esp32/src/communication/wlan/wlan.cpp
@@ -1,23 +1,19 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-#define CHASER 0
-#define CHASED 1
-
-#define ROLE CHASED
-
 char ssid[] = "ESP32";
 char password[] = "12345678";
 
 namespace WLAN
 {
-    // Is called on its own
-    void setup()
+    void setup_AP()
     {
-#if ROLE == CHASED
         WiFi.softAP(ssid, password); // Create accesspoint
-#else
-        WiFi.begin(ssid, password); // Connect to the accesspoint on chased-one
+    }
+
+    void setup_Client()
+    {
+        WiFi.begin(ssid, password); // Connect to the accesspoint
         Serial.println("Connecting to WiFi:");
         while (WiFi.status() != WL_CONNECTED) // Is the WiFi connection successful
         {
@@ -26,6 +22,5 @@ namespace WLAN
         }
         Serial.println("\nWiFi connected!");
         WiFi.setAutoReconnect(true);
-#endif
     }
 } // namespace WLAN
